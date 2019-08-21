83dbe7888e434e181747dc25e0eacb6769bd2248
JonatanRek <gamerclassn7@gmail.com>
2019-08-21 11:47:41 +0200

    Require parametr

--- Form.php
+++ Form.php
@@ -54,14 +54,15 @@ class Form {
 	 * @param String     $id      id of element
 	 * @param String     $label   label of element
 	 * @param String     $value   value of element
+	 * @param boolean    $require require selector toggle
 	 * @param boolean    $enabled enable selector toggle
 	 */
-	function addInput(String $type, String $name, String $id, String $label, String $value, Bool $enabled = true){
+	function addInput(String $type, String $name, String $id, String $label, String $value, Bool $require = false, Bool $enabled = true){
 		$this->formContent .= '<div class="field">';
 		if ($label != "") {
 			$this->formContent .= '<div class="label">'.$label.'</div>';
 		}
-		$this->formContent .= '<input class="input" type="'.$type.'" name="'.$name.'" value="'.$value.'" ' . ($enabled ? '' : 'disabled') . '>';
+		$this->formContent .= '<input class="input" type="'.$type.'" name="'.$name.'" value="'.$value.'" ' . ($enabled ? '' : 'disabled')  . ($require ? '' : 'required') .'>';
 		$this->formContent .= '</div>';
 	}
 
@@ -75,12 +76,12 @@ class Form {
 	 * @param boolean $multiple multiple selector toggle
 	 * @param boolean $enabled  enable selector toggle
 	 */
-	function addSelect(String $name, String $id, String $label, Array $data, Bool $multiple = false, Bool $enabled = true){
+	function addSelect(String $name, String $id, String $label, Array $data, Bool $multiple = false, Bool $require = false, Bool $enabled = true){
 		$this->formContent .= '<div class="field">';
 		if ($label != "") {
 			$this->formContent .= '<div class="label">'.$label.'</div>';
 		}
-		$this->formContent .= '<select class="input"' . ($multiple ? '' : 'multiple') . ($enabled ? '' : 'disabled') . '>';
+		$this->formContent .= '<select class="input"' . ($multiple ? '' : 'multiple') . ($enabled ? '' : 'disabled') . ($require ? '' : 'required') .'>';
 		foreach ($data as $value => $text) {
 			$this->formContent .= '<option value="' . $value . '">' . $text . '</option>';
 		}
@@ -96,12 +97,12 @@ class Form {
 	 * @param String     $value   value of element
 	 * @param boolean    $enabled enable selector toggle
 	 */
-	function addTextarea(String $name, String $id, String $label, Array $value, Bool $enabled = true){
+	function addTextarea(String $name, String $id, String $label, Array $value, Bool $require = false, Bool $enabled = true){
 		$this->formContent .= '<div class="field">';
 		if ($label != "") {
 			$this->formContent .= '<div class="label">'.$label.'</div>';
 		}
-		$this->formContent .= '<textarea class="input"'  . ($enabled ? '' : 'disabled') . '>';
+		$this->formContent .= '<textarea class="input"'  . ($enabled ? '' : 'disabled')  . ($require ? '' : 'required') .'>';
 		$this->formContent .= $value;
 		$this->formContent .= '</textarea>';
 		$this->formContent .= '</div>';
